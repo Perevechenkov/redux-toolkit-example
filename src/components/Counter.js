1e5267e919b15d53463dfac67ca491ed8bc19da4
DesperateHiking <perevechenkov_1999@mail.ru>
2022-05-29 23:40:18 +0500

    using redux state & dispatching actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.scss';
 
+import { increment, decrement } from './counterSlice';
+
 export default function Counter(props) {
-  const toggleCounterHandler = () => {};
+  const counterValue = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
+  const increaseCounterHandler = () => {
+    dispatch(increment());
+  };
+  const decreaseCounterHandler = () => {
+    dispatch(decrement());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>Value</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes.value}>{counterValue}</div>
+      <button onClick={increaseCounterHandler}>Increase</button>
+      <button onClick={decreaseCounterHandler}>Decrease</button>
     </main>
   );
 }
