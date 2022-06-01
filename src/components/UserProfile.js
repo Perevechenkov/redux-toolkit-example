7a0a18a98e4a1b450ddcf142711255a07544fd95
DesperateHiking <perevechenkov_1999@mail.ru>
2022-06-01 21:00:01 +0500

    userProfile dummy component

---
+++ src/components/UserProfile.js
@@ -0,0 +1,9 @@
+import classes from './UserProfile.module.scss';
+
+export default function UserProfile(props) {
+  return (
+    <main className={classes.profile}>
+      <h2>User profile</h2>
+    </main>
+  );
+}
