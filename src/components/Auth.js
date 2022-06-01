781eaf8a39d3356e5ecd67e6e702fac9cecf4965
DesperateHiking <perevechenkov_1999@mail.ru>
2022-06-01 21:01:10 +0500

    using auth actions to simulate login

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { login, logout } from './authSlice';
+
 import classes from './Auth.module.scss';
 
 export default function Auth(props) {
+  const dispatch = useDispatch();
+
+  const loginHandler = event => {
+    event.preventDefault();
+
+    dispatch(login());
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { login, logout } from './authSlice';
+
 import classes from './Header.module.scss';
 
 export default function Header(props) {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const logoutHandler = event => {
+    event.preventDefault();
+
+    dispatch(logout());
+  };
+
   return (
     <header className={classes.header}>
       <h2>Redux Auth</h2>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
