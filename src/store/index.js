8b4052879eb5287aa297a478e1cb0bf572c02f7f
DesperateHiking <perevechenkov_1999@mail.ru>
2022-06-01 21:00:38 +0500

    authSlice with login & logout actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../components/counterSlice';
+import authReducer from '../components/authSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    auth: authReducer,
   },
 });
